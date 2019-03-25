3e67abc93d9263f6885a1e16f1bad1f6e5e4cbf0
jordanhugh <jordanhu@tcd.ie>
2019-03-25 12:13:55 +0000

    Basic Socket Interface

--- my-router.cpp
+++ my-router.cpp
@@ -17,5 +17,33 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	// Create a socket using UCP IP
+    int sockfd;
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		perror("createsocket");
+		return 2;
+	}
+
+    // Allow others to reuse the address
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
+        return 3;
+    }
+
+    // Bind address to socket
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(40000); // Short, network byte order
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        return 4;
+    }
+
+
+
 	return 0;
 }
